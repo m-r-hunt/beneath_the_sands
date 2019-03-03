5f3e60993fc8808514c1eda724b9a59e0ed701cf
Max Hunt <max@huntw3.com>
2019-03-03 17:33:31 +0000

    Random cycle generation working-ish.

--- src/level_generation.rs
+++ src/level_generation.rs
@@ -37,14 +37,91 @@ fn carve_room(position: (i32, i32), size: (i32, i32), tile_map: &mut TileMap) {
     }
 }
 
+fn manhatten_distance(from: (i32, i32), to: (i32, i32)) -> i32 {
+    (from.0 - to.0).abs() + (from.1 - to.1).abs()
+}
+
 pub fn generate_level() -> GeneratedLevel {
     let mut rng = rand::thread_rng();
     let mut tile_map: TileMap = Default::default();
     // Assume start position is always 0, 0
     // Pick an end position
+    let start = (0, 0);
     let end = (rng.gen_range(1, 5), rng.gen_range(0, 5));
-    carve_room((0, 0), (10, 10), &mut tile_map);
-    carve_room(end, (10, 10), &mut tile_map);
+    let mut path = vec![start];
+    let mut current_pos = start;
+    while current_pos != end {
+        let mut choices = Vec::new();
+        for choice in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+            if path
+                .iter()
+                .any(|c| *c == (current_pos.0 + choice.0, current_pos.1 + choice.1))
+            {
+                continue;
+            }
+
+            choices.push(choice);
+
+            if manhatten_distance((current_pos.0 + choice.0, current_pos.1 + choice.1), end)
+                < manhatten_distance(current_pos, end)
+            {
+                choices.push(choice);
+            }
+        }
+
+        assert!(!choices.is_empty());
+
+        let index = rng.gen_range(0, choices.len());
+        let choice = choices[index];
+        current_pos = (current_pos.0 + choice.0, current_pos.1 + choice.1);
+        path.push(dbg!(current_pos));
+    }
+
+    while manhatten_distance(current_pos, start) != 1 {
+        let mut choices = Vec::new();
+        for choice in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+            if path
+                .iter()
+                .any(|c| *c == (current_pos.0 + choice.0, current_pos.1 + choice.1))
+            {
+                continue;
+            }
+
+            choices.push(choice);
+
+            if manhatten_distance((current_pos.0 + choice.0, current_pos.1 + choice.1), start)
+                < manhatten_distance(current_pos, start)
+            {
+                choices.push(choice);
+            }
+        }
+
+        assert!(!choices.is_empty());
+
+        let index = rng.gen_range(0, choices.len());
+        let choice = choices[index];
+        current_pos = (current_pos.0 + choice.0, current_pos.1 + choice.1);
+        path.push(dbg!(current_pos));
+    }
+    path.push(start);
+
+    for room in path.iter() {
+        carve_room(*room, (10, 10), &mut tile_map);
+    }
+    for i in 0..path.len() - 1 {
+        let dx = path[i + 1].0 - path[i].0;
+        let dy = path[i + 1].1 - path[i].1;
+        let steps = (dx * 20).abs().max((dy * 20).abs());
+        for step in 0..steps {
+            tile_map.tiles.insert(
+                (path[i].0 * 20 + step * dx, path[i].1 * 20 + step * dy),
+                Tile {
+                    collision: false,
+                    colour: rgba!(223, 201, 96, 1.0),
+                },
+            );
+        }
+    }
     GeneratedLevel {
         tile_map,
         start_position: (5, 5),
