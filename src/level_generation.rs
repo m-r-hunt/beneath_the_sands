2342deab85dcf02863a2813f1d438c3d56215802
Max Hunt <max@huntw3.com>
2019-03-05 08:25:15 +0000

    Start improving level generation. Use simpler initial random walk, limit length and start to add side paths

--- src/level_generation.rs
+++ src/level_generation.rs
@@ -1,5 +1,6 @@
 use crate::physics::{Tile, TileMap};
 use rand::Rng;
+use std::collections::HashSet;
 
 // # S-#-#-#
 //   =   o |
@@ -62,26 +63,19 @@ fn try_generate_level() -> Result<GeneratedLevel, StringErr> {
     // Assume start position is always 0, 0
     // Pick an end position
     let start = (0, 0);
-    let end = (rng.gen_range(1, 5), rng.gen_range(0, 5));
     let mut path = vec![start];
+    let mut visited = HashSet::new();
+    visited.insert(start);
     let mut current_pos = start;
-    while current_pos != end {
+    let initial_path_len = rng.gen_range(3, 6);
+    for _ in 0..initial_path_len {
         let mut choices = Vec::new();
         for choice in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
-            if path
-                .iter()
-                .any(|c| *c == (current_pos.0 + choice.0, current_pos.1 + choice.1))
-            {
+            if visited.contains(&(current_pos.0 + choice.0, current_pos.1 + choice.1)) {
                 continue;
             }
 
             choices.push(choice);
-
-            if manhatten_distance((current_pos.0 + choice.0, current_pos.1 + choice.1), end)
-                < manhatten_distance(current_pos, end)
-            {
-                choices.push(choice);
-            }
         }
 
         if choices.is_empty() {
@@ -93,15 +87,45 @@ fn try_generate_level() -> Result<GeneratedLevel, StringErr> {
         let choice = choices[index];
         current_pos = (current_pos.0 + choice.0, current_pos.1 + choice.1);
         path.push(current_pos);
+        visited.insert(current_pos);
     }
+
+    let choice_point = path[rng.gen_range(2, path.len() - 1)];
+    let mut side_path = vec![choice_point];
+    {
+        let mut current_pos = choice_point;
+        let side_path_len = rng.gen_range(1, 3);
+        for _ in 0..side_path_len {
+            let mut choices = Vec::new();
+            for choice in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+                if visited.contains(&(current_pos.0 + choice.0, current_pos.1 + choice.1)) {
+                    continue;
+                }
+
+                choices.push(choice);
+            }
+
+            if choices.is_empty() {
+                return Err(StringErr(
+                    "Got stuck with no choice side loop 1".to_string(),
+                ));
+            }
+            assert!(!choices.is_empty());
+
+            let index = rng.gen_range(0, choices.len());
+            let choice = choices[index];
+            current_pos = (current_pos.0 + choice.0, current_pos.1 + choice.1);
+            side_path.push(current_pos);
+            visited.insert(current_pos);
+        }
+    }
+
+    let end = current_pos;
 
     while manhatten_distance(current_pos, start) != 1 {
         let mut choices = Vec::new();
         for choice in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
-            if path
-                .iter()
-                .any(|c| *c == (current_pos.0 + choice.0, current_pos.1 + choice.1))
-            {
+            if visited.contains(&(current_pos.0 + choice.0, current_pos.1 + choice.1)) {
                 continue;
             }
 
@@ -122,10 +146,17 @@ fn try_generate_level() -> Result<GeneratedLevel, StringErr> {
         let choice = choices[index];
         current_pos = (current_pos.0 + choice.0, current_pos.1 + choice.1);
         path.push(current_pos);
+        visited.insert(current_pos);
+    }
+    if path.len() >= 10 {
+        return Err(StringErr("Initial path excessively long".to_string()));
     }
     path.push(start);
 
     for room in path.iter() {
+        carve_room(*room, (10, 10), &mut tile_map);
+    }
+    for room in side_path.iter() {
         carve_room(*room, (10, 10), &mut tile_map);
     }
     for i in 0..path.len() - 1 {
@@ -223,6 +254,102 @@ fn try_generate_level() -> Result<GeneratedLevel, StringErr> {
             }
         }
     }
+    for i in 0..side_path.len() - 1 {
+        let dx = side_path[i + 1].0 - side_path[i].0;
+        let dy = side_path[i + 1].1 - side_path[i].1;
+        if dx != 0 {
+            let steps = (dx * 20).abs() - 10;
+            for step in 0..steps {
+                let offset = if dx == 1 { 10 } else { -1 };
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + offset + step * dx,
+                        side_path[i].1 * 20 + 4 + step * dy,
+                    ),
+                    Tile {
+                        collision: true,
+                        colour: rgba!(128, 128, 128, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + offset + step * dx,
+                        side_path[i].1 * 20 + 5 + step * dy,
+                    ),
+                    Tile {
+                        collision: false,
+                        colour: rgba!(223, 201, 96, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + offset + step * dx,
+                        side_path[i].1 * 20 + 6 + step * dy,
+                    ),
+                    Tile {
+                        collision: false,
+                        colour: rgba!(223, 201, 96, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + offset + step * dx,
+                        side_path[i].1 * 20 + 7 + step * dy,
+                    ),
+                    Tile {
+                        collision: true,
+                        colour: rgba!(128, 128, 128, 1.0),
+                    },
+                );
+            }
+        } else {
+            let steps = (dy * 20).abs() - 10;
+            for step in 0..steps {
+                let offset = if dy == 1 { 10 } else { -1 };
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + 4 + step * dx,
+                        side_path[i].1 * 20 + offset + step * dy,
+                    ),
+                    Tile {
+                        collision: true,
+                        colour: rgba!(128, 128, 128, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + 5 + step * dx,
+                        side_path[i].1 * 20 + offset + step * dy,
+                    ),
+                    Tile {
+                        collision: false,
+                        colour: rgba!(223, 201, 96, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + 6 + step * dx,
+                        side_path[i].1 * 20 + offset + step * dy,
+                    ),
+                    Tile {
+                        collision: false,
+                        colour: rgba!(223, 201, 96, 1.0),
+                    },
+                );
+                tile_map.tiles.insert(
+                    (
+                        side_path[i].0 * 20 + 7 + step * dx,
+                        side_path[i].1 * 20 + offset + step * dy,
+                    ),
+                    Tile {
+                        collision: true,
+                        colour: rgba!(128, 128, 128, 1.0),
+                    },
+                );
+            }
+        }
+    }
+
     let exit_position = (end.0 * 20 + 5, end.1 * 20 + 5);
     Ok(GeneratedLevel {
         tile_map,
